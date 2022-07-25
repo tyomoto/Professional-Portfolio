f0c50f24c4cd86135f8a2de6958a5f5e9e23d02f
T Omoto <tyler.omoto@gmail.com>
2022-07-25 10:15:46 -0400

    updated aboutme text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ export default function AboutMe(){
             <h1>Tyler Omoto</h1>
             <img src={profilePic} alt="Tyler"/>
             <p>Hello! I'm Tyler Omoto, currently studying to be a certified full-stack developer with the University of Toronto full-stack coding bootcamp. I am looking to 
-                leverage my business/marketing background to help bridge the gap between consumer/client and the technical side. Through the bootcamp I have learned the fundamentals
+                leverage my 2 years of business/marketing experience to help bridge the gap between consumer/client and the technical side. Through the bootcamp I have learned the fundamentals
                 of HTML, CSS, and Javascript, as well as some experience in NodeJS, Expressjs, SQL and React.  Through-out the course I have worked on developing my 
                 problem solving and creativity while continuing to further develop my teamwork and organization skills. I have successfully completed several team projects in the class, that 
                 all successfully run and allow for an easy user experience. The most recent project we built is a platform that allows for users to login and leave a review or comment on a movie. 
