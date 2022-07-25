e3d4b112ee5996ec8605c2520656ad5383470d98
T Omoto <tyler.omoto@gmail.com>
2022-07-25 12:49:13 -0400

    added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Nav({currentPage, handlePageChange}) {
+    return(
+        <div className='navigation'>
+            <nav>
+                <header className= 'header'>
+                    <h1>Tyler Omoto</h1>
+                </header>
+                
+                <a href="#AboutMe" onClick={() => handlePageChange('AboutMe')} className={currentPage === 'AboutMe' ? 'active' : 'nav'}> About Me</a>
+                <a href="#Projects" onClick={() => handlePageChange('Projects')} className={currentPage === 'Projects' ? 'active' : 'nav'}> Portfolio</a>
+                <a href="#Contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'active' : 'nav'}> Contact</a>
+                <a href="#Contact" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'active' : 'nav'}> Resume</a>
+            </nav>
+        </div>
+  );
+}
