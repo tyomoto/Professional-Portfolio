ad8e1abca871f5a246cdcaa41a2420e18cd9b76c
T Omoto <tyler.omoto@gmail.com>
2022-07-26 04:28:09 -0400

    added footer styling and content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import '../styles/Footer.css'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 
 export default function Footer() {
     return(
        <div className='footer'>
-
+            <p>
+                <a href="https://github.com/tyomoto" target = "_blank" rel = "noreferrer">
+                    <GitHubIcon color = "action" fontSize = "large"/>
+                </a>
+                <a href="https://www.linkedin.com/in/tyleromoto/" target = "_blank" rel = "noreferrer">
+                    <LinkedInIcon color = "action" fontSize = "large"/>
+                </a>
+                <EmailIcon color= "action" fontSize = "large"/> : tyler.omoto@gmail.com
+        </p>
        </div> 
     )
 }
